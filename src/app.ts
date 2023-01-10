02554027e98810680773969840fba17ba494ff51
Josiel Luz <josiel9511791722@gmail.com>
2023-01-10 11:13:29 -0300

    feat: add handleError

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import "express-async-errors";
 import express from "express";
+import handleError from "./errors/handleError";
 
 const app = express();
 app.use(express.json());
+
+app.use(handleError);
 
 app.get("/", (req, res) => {
   console.log("helo world");
