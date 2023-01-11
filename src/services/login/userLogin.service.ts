9f99eb9d16b5650fb810ef1cf4497a6a848c82b4
Matheus Carneiro <mtscrn21@gmail.com>
2023-01-11 12:00:33 -0300

    Create: Login Middleware, login Routes

--- src/services/login/userLogin.service.ts
+++ src/services/login/userLogin.service.ts
@@ -5,26 +5,12 @@ import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
 
-const userLoginService =async ({email, password}) => {
+const userLoginService =async ({email}) => {
     const usersRepository = AppDataSource.getRepository(User)
 
     const user = await usersRepository.findOneBy({
         email: email 
     })
-
-    if(!user){
-        throw new AppError("User or password invalid!", 403)
-    }
-    
-    if(user.isActive === false){
-        throw new AppError("The user is not active", 400)
-    }
-
-    const passwordMatch = await compare(password, user.password)
-
-    if(!passwordMatch){
-        throw new AppError("User or password invalid!", 403)
-    }
 
     const token = jwt.sign(
         {
