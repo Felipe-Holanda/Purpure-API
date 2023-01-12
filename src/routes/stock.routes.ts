0de81561af562d9c61ab1679932f9974f0ded778
Josiel Luz <josiel9511791722@gmail.com>
2023-01-12 14:47:09 -0300

    insert ensureAuthMiddleware in stockRouter

--- src/routes/stock.routes.ts
+++ src/routes/stock.routes.ts
@@ -7,6 +7,7 @@ import {
   updateStockController,
 } from "../controllers/stock.controller";
 import verifySchemaMiddleware from "../middlewares/global/verifySchema.middleware";
+import { ensureAuthMiddleware } from "../middlewares/login/ensureAuth.middleware";
 import verifyStockAlreadyExistsMiddleware from "../middlewares/stock/verifyStockAlreadyExists.middleware";
 import verifyStockexistsMiddleware from "../middlewares/stock/verifyStockExists.middleware";
 import {
@@ -21,24 +22,37 @@ const stockRouter = Router();
 
 stockRouter.post(
   "",
+  ensureAuthMiddleware,
   verifySchemaMiddleware(stockRequestEschema),
   verifyStockAlreadyExistsMiddleware,
   createStockController
 );
-stockRouter.get("", listStockController);
-stockRouter.get("/:id", verifyStockexistsMiddleware, getStockByIdController);
+stockRouter.get("", ensureAuthMiddleware, listStockController);
+stockRouter.get(
+  "/:id",
+  ensureAuthMiddleware,
+  verifyStockexistsMiddleware,
+  getStockByIdController
+);
 stockRouter.patch(
   "/:id",
+  ensureAuthMiddleware,
   verifySchemaMiddleware(stockUpdateSchema),
   verifyStockexistsMiddleware,
   updateStockController
 );
 stockRouter.put(
   "/:id",
+  ensureAuthMiddleware,
   verifySchemaMiddleware(insertStockSchema),
   verifyStockexistsMiddleware,
   updateStockController
 );
-stockRouter.delete("/:id", verifyStockexistsMiddleware, deleteStockController);
+stockRouter.delete(
+  "/:id",
+  ensureAuthMiddleware,
+  verifyStockexistsMiddleware,
+  deleteStockController
+);
 
 export default stockRouter;
