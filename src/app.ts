2c1f16364380313cd569553bde376aff8047a5ce
Felipe Holanda <93724958+Felipe-Holanda@users.noreply.github.com>
2023-01-12 10:14:33 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,6 @@ import { loginRoutes } from "./routes/login.routes";
 const app = express();
 app.use(express.json());
 
-app.use(handleError);
-
-app.get("/", (req, res) => {
-  console.log("helo world");
-
-  return res.json("helo world");
-});
-
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
 
