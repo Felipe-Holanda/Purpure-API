6dbd902d62b01344a95d21e1ac5f4ea159d4bbc3
Gabriel Gustavo Dias Barreto <106823099+GabrielGustavoDias@users.noreply.github.com>
2023-01-16 09:09:47 -0300

    midd insert

--- src/middlewares/login/userIsActive.midleware.ts
+++ src/middlewares/login/userIsActive.midleware.ts
@@ -1,23 +1,24 @@
-import { NextFunction, Request, Response } from "express"
-import AppDataSource from "../../data-source"
-import { User } from "../../entities/users.entity"
-import AppError from "../../errors/AppError"
+import { NextFunction, Request, Response } from 'express'
+import AppDataSource from '../../data-source'
+import { User } from '../../entities/users.entity'
+import AppError from '../../errors/AppError'
 
+const userIsActive = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const usersRepository = AppDataSource.getRepository(User)
 
+  const user = await usersRepository.findOneBy({
+    email: req.body.email,
+  })
 
-const userIsActive =async (req:Request, res: Response, next: NextFunction) => {
-    
-    const usersRepository = AppDataSource.getRepository(User)
+  if (!user) {
+    throw new AppError('The user is not active', 400)
+  }
 
-    const user = await usersRepository.findOneBy({
-        email: req.body.email 
-    })
-
-    if(!user){
-        throw new AppError("The user is not active", 400)
-    }
-
-    return next()
+  return next()
 }
 
 export default userIsActive
