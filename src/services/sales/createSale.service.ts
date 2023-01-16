235632ae99f74a9a09aacfc42a6cebebd79af745
Gabriel Gustavo Dias Barreto <106823099+GabrielGustavoDias@users.noreply.github.com>
2023-01-16 11:59:14 -0300

    verify client id middleware

--- src/services/sales/createSale.service.ts
+++ src/services/sales/createSale.service.ts
@@ -1,31 +1,22 @@
 import AppDataSource from '../../data-source'
 import { Sales } from '../../entities/sales.entity'
-import { ISales, ISalesReturn } from '../../interfaces/sales.interfaces'
-// import {Clients} from '../../entities/clients.entity'
+import { Clients } from '../../entities/clients.entity'
 
 export const createSaleService = async (body): Promise<Sales[]> => {
   const salesRepository = AppDataSource.getRepository(Sales)
 
-  // const clientsRespository = AppDataSource.getRepository(Clients)
+  const clientsRespository = AppDataSource.getRepository(Clients)
 
-  // const foundClient = clientsRespository.findOneBy({ id: body.id })
+  const foundClient = await clientsRespository.findOneBy({ id: body.id })
 
   const createdSale = salesRepository.create(body)
   await salesRepository.save(createdSale)
 
-  // const returnedSale = {
-  //   id: body.id,
-  //   client_name: foundClient.name,
-  //   amount: body.amount,
-  //   value: body.value,
-  //   createdAt: body.createdAt,
-  // }
+  const reuturnedSale = {
+    ...body,
+    clientId: undefined,
+    client_name: foundClient.name,
+  }
 
-  // const reuturnedSale = {
-  //   ...body,
-  //   clientId: undefined,
-  //   client_name: foundClient.name,
-  // }
-
-  return createdSale
+  return reuturnedSale
 }
